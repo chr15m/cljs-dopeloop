4c33e5d29bf9a1698b6b26d5d41e446b4cfdd4f5
Chris McCormick <chris@mccormick.cx>
2023-10-16 14:14:22 +0800

    Add an audio margin (pad change into future) to eliminate clicks.

--- src/dopeloop/main.cljs
+++ src/dopeloop/main.cljs
@@ -46,10 +46,10 @@
 
 (defn loop-audio-buffer!
   "Starts an AudioBuffer looping from the position `start-at`."
-  [ctx audio-buffer & [start-at]]
+  [ctx audio-buffer & [start-at audio-margin]]
   (let [src (.createBufferSource ctx)
         start-at (or start-at 0)
-        start-when (+ (aget ctx "currentTime") 0.00)]
+        start-when (+ (aget ctx "currentTime") audio-margin)]
     (aset src "buffer" audio-buffer)
     (.connect src (aget ctx "destination"))
     (aset src "loop" true)
@@ -59,21 +59,23 @@
 
 (defn stop-source!
   "Stops an AudioBufferSourceNode and disconnects it from the audio graph."
-  [src]
+  [src & [stop-at]]
   (when src
-    (.disconnect src)
-    (.stop src 0))
+    (.stop src (or stop-at 0))
+    (js/setTimeout #(.disconnect src) (inc (* (or stop-at 0) 1000))))
   nil)
 
 (defn compute-seamless-loop-offset
   "Compute the offset to start a new loop from so that it matches the old loop.
   The two loops can have different counts of beats in them and be different BPMs.
   Args are the same as `seamless-loop-audio-buffer!`."
-  [ctx audio-buffer playing-source old-beats-count new-beats-count]
+  [ctx audio-buffer playing-source old-beats-count new-beats-count & [audio-margin]]
   (when playing-source
     (let [new-duration (aget audio-buffer "duration")
           old-duration (aget playing-source "buffer" "duration")
-          offset (mod (- (aget ctx "currentTime") (aget playing-source "t_start")) old-duration)
+          offset (mod (- (+ (aget ctx "currentTime")
+                            (or audio-margin 0))
+                         (aget playing-source "t_start")) old-duration)
           offset-in-old-slices (/ offset (/ old-duration old-beats-count))
           offset-in-new-slices (mod offset-in-old-slices new-beats-count)
           offset-rejigged (* offset-in-new-slices (/ new-duration new-beats-count))]
@@ -87,11 +89,11 @@
   * `playing-source` is the already playing AudioBufferSourceNode which will be ignored if nil.
   * `old-beats-count` is optionally the number of beats in the playing loop, defaults to 1.
   * `new-beats-count` is optionally the number of beats in the new loop, defaults to 1."
-  [ctx audio-buffer playing-source & [old-beats-count new-beats-count]]
-  (let [offset (compute-seamless-loop-offset ctx audio-buffer playing-source (or old-beats-count 1) (or new-beats-count 1))
-        src (loop-audio-buffer! ctx audio-buffer offset)]
-    (when playing-source
-      (stop-source! playing-source))
+  [ctx audio-buffer playing-source & [old-beats-count new-beats-count audio-margin-ms]]
+  (let [audio-margin (-> audio-margin-ms (or 0) (/ 1000))
+        offset (compute-seamless-loop-offset ctx audio-buffer playing-source (or old-beats-count 1) (or new-beats-count 1) audio-margin)
+        src (loop-audio-buffer! ctx audio-buffer offset audio-margin)]
+    (stop-source! playing-source (+ (aget ctx "currentTime") (or audio-margin 0)))
     src))
 
 (defn get-loop-position
